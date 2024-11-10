de503ea63d5ab0d9750e3222d4648fdd5d1cfade
Hon139 <83674184+Hon139@users.noreply.github.com>
2024-11-10 04:18:59 -0500

    captcha

--- auth-frontend/src/pages/captcha.js
+++ auth-frontend/src/pages/captcha.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import logo from '../components/YorkU_logo.png';
 import './pages.css';
-
-function Captcha() {
-  
+import ReCAPTCHA from 'react-google-recaptcha';
+function Captcha(){
+  const [capVal, setCapVal] = useState(null);
   return (
     <div className="MathAuth">
       <header className="Banner_Logo">
@@ -13,8 +13,13 @@ function Captcha() {
       <div className="header-banner">
         <h1 className="passport-text">Passport York Login</h1>
       </div>
+        <div> TITLE</div>
+        <ReCAPTCHA 
+        sitekey="6Ldtb3oqAAAAAAQI2IySLzcTZuA9QxA6-tNUrRbF"
+        onChange={(val) => setCapVal(val)}
+        />
 
-    
+        <button > Submit</button>
     </div>
   );
 }
