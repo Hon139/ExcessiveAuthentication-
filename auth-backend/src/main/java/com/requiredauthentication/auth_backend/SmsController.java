6c1f643d1fa5847090847c3e67b5ee755c839ec9
TaoEvan <tao.evan.pi@gmail.com>
2024-11-10 08:40:26 -0500

    sms stuff

--- auth-backend/src/main/java/com/requiredauthentication/auth_backend/SmsController.java
+++ auth-backend/src/main/java/com/requiredauthentication/auth_backend/SmsController.java
@@ -1,6 +1,7 @@
 package com.requiredauthentication.auth_backend;
 
 import com.requiredauthentication.auth_backend.SmsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Random;
 
+@Slf4j
 @RestController
 @RequestMapping("/sms")
 public class SmsController {
@@ -22,6 +24,7 @@ public class SmsController {
         verificationCode = rand.nextInt(1000000);
 
         System.out.println(number);
+        log.info("sent code to " + number);
         return smsService.sendSms(number, "Your verification code is: " + verificationCode);
     }
 
@@ -32,5 +35,20 @@ public class SmsController {
         } else {
             return "Invalid Code!";
         }
+    }
+
+    @PostMapping("/receive")
+    public String receiveSms(@RequestParam Map<String, String> messageData) {
+        String from = messageData.get("From");  // Sender's phone number
+        String to = messageData.get("To");      // Your SignalWire number
+        String body = messageData.get("Body");  // The message content
+
+        System.out.println("Received SMS from: " + from);
+        System.out.println("Message content: " + body);
+        log.info("Received SMS from: " + from);
+
+        // Process the message as needed (e.g., save it to a database, respond, etc.)
+        smsService.sendSms(from, "Message Received");
+        return "Message received successfully";
     }
 }
